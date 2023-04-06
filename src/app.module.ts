44b8327a0993b447c575b4d05f496278ac8dba48
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2023-04-06 22:16:44 +0500

    feat: integrated devtools

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import config from './config';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
 
 @Module({
   imports: [
+    DevtoolsModule.register({
+      http: process.env.NODE_ENV !== 'prod',
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       load: [config],
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    snapshot: true,
+  });
   app.enableCors();
   app.setGlobalPrefix('/v1/api');
   const config = new DocumentBuilder()
